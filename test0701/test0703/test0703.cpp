0f84f22805d2e3b4204a4dd3b9c272e9ff282fdf
TeerawatMut <6812100022@mut.ac.th>
2025-12-28 12:20:29 +0700

    lab07

---
+++ test0701/test0703/test0703.cpp
@@ -0,0 +1,14 @@
+// TODO: ?????????? a ???? pointer
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5;
+    int* p = &a;
+
+    // TODO
+	*p = 100
+        ;
+    cout << "a = " << a << endl;
+    return 0;
+}
---
+++ test0701/test0706/test0706.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+void printArray(int* p, int size) {
+    for (int i = 0; i < size; i++) {
+        cout << *(p + i) << " ";
+    }
+    cout << endl;
+}
+
+void swapValue(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void bubbleSortDescending(int* p, int size) {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - 1 - i; j++) {
+            if (*(p + j) < *(p + j + 1)) {
+                swapValue(p + j, p + j + 1);
+            }
+        }
+    }
+}
+
+int main() {
+    int arr[] = { 5, 2, 9, 1, 3 };
+    int size = 5;
+
+    cout << "Before:";
+    printArray(arr, size);
+
+    bubbleSortDescending(arr, size);
+
+    cout << "After:";
+    printArray(arr, size);
+
+    return 0;
+}
